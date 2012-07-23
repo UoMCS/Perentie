bc364cf4f7ed0d7c28f69f34663efd2283f1679a
Jonathan Heathcote <mail@jhnet.co.uk>
2012-07-23 17:40:14 +0100

    Defined disassembler interface.

---
+++ system/disassembler/disassembler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+The interface expected for disassemblers.
+"""
+
+
+class Disassembler(object):
+	
+	def __init__(self):
+		pass
+	
+	
+	def disassemble(self, memory_interface, program_start_addr, start_addr, num_instrs):
+		"""
+		Given the starting address of a program, disassemble num_instrs starting
+		from start_addr as found in memory_interface. Should return a list of
+		num_instrs tuples (num_instr_bits, instr_asm).
+		
+		The memory interface should provide:
+		memory_interface.read(addr, word_width, num_words) -> str (bytes)
+		"""
+		raise NotImplementedError("Disassembler not implemented!")
+
