adcb5ca196cb7356b00c58479b83af06d724aea8
Jonathan Heathcote <mail@jhnet.co.uk>
2012-07-23 13:27:00 +0100

    Added serial backend.

---
+++ backend/serial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+A serial-port back-end for the protocol.
+"""
+
+from serial import Serial
+
+
+class SerialBackend(object):
+	
+	def __init__(self, port = None, baudrate = 115200,
+	             read_timeout = 1.0, write_timeout = 0.1):
+		"""
+		Provides a serial-port back-end for the protocol. Timeouts are in seconds.
+		"""
+		
+		# Start the emulator as a child-process
+		self.serial = Serial(port,
+		                     baudrate     = baudrate,
+		                     timeout      = read_timeout,
+		                     writeTimeout = write_timeout)
+	
+	
+	def read(self, length):
+		return self.serial.read(length)
+	
+	
+	def write(self, data):
+		self.serial.write(data)
+	
+	
+	def flush(self):
+		self.serial.flush()
+
