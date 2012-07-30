b4ca32a42d24133719e49302ce9491dfe47b8554
Jonathan Heathcote <mail@jhnet.co.uk>
2012-07-30 15:13:09 +0100

    Added main GUI.

---
+++ main.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+
+"""
+Debugger GUI main executable.
+"""
+
+import pygtk, gtk, glib, gobject
+
+from back_end import EmulatorBackEnd, SerialPortBackEnd
+from system   import System
+
+from view.register    import RegisterViewer
+from view.memory      import MemoryViewer
+from view.control_bar import ControlBar
+from view.log         import LogViewer
+
+
+class Main(gtk.Window):
+	
+	# Number of ms between screen refreshes
+	REFRESH_INTERVAL = 100
+	
+	def __init__(self, system):
+		"""
+		A quick-and-simple main debugger window containing a control bar, register
+		viewer, two memory viewers and a log viewer.
+		"""
+		gtk.Window.__init__(self, gtk.WINDOW_TOPLEVEL)
+		
+		self.system = system
+		
+		# Set the window title
+		self.set_title("%s Debugger (%s)"%(
+			self.system.architecture.name, self.system.back_end.name
+		))
+		
+		# Default window size
+		self.set_default_size(800, 600)
+		
+		self.vbox = gtk.VBox()
+		self.add(self.vbox)
+		
+		# Add the control bar
+		self.control_bar = ControlBar(self.system)
+		self.vbox.pack_start(self.control_bar, expand = False, fill = True)
+		
+		# Add a movable division between the register and memory viewers
+		self.hpaned = gtk.HPaned()
+		self.vbox.pack_start(self.hpaned, expand = True, fill = True)
+		
+		# Add a register viewer
+		self.register_viewer = RegisterViewer(self.system)
+		self.hpaned.pack1(self.register_viewer, shrink = False)
+		
+		
+		# Add a movable division between the two memory viewers
+		self.vpaned = gtk.VPaned()
+		self.hpaned.pack2(self.vpaned, shrink = False)
+		
+		# Add two memory viewers (one defaulting to a disassembly, one not)
+		memory = self.system.architecture.memories[0]
+		self.memory_viewers = []
+		self.memory_viewers.append(MemoryViewer(self.system, memory, True))
+		self.memory_viewers.append(MemoryViewer(self.system, memory, False))
+		
+		# Show memory viewers in frames
+		f1 = gtk.Frame()
+		f2 = gtk.Frame()
+		f1.add(self.memory_viewers[0])
+		f2.add(self.memory_viewers[1])
+		f1.set_shadow_type(gtk.SHADOW_OUT)
+		f2.set_shadow_type(gtk.SHADOW_OUT)
+		self.vpaned.pack1(f1, resize = True, shrink = False)
+		self.vpaned.pack2(f2, resize = True, shrink = False)
+		
+		# Add an expander with a log viewer in
+		self.unread_log_entries = 0
+		self.log_expander = gtk.Expander("Error Log")
+		self.log_viewer   = LogViewer(self.system)
+		self.log_expander.add(self.log_viewer)
+		self.log_viewer.connect("update", self._on_log_update)
+		self.log_expander.connect("activate", self._on_log_update, None)
+		self.log_expander.set_use_markup(True)
+		self.vbox.pack_start(self.log_expander, expand = False, fill = True)
+		
+		# Interval at which to auto refresh the UI's contents
+		glib.timeout_add(Main.REFRESH_INTERVAL, self._on_interval)
+		
+		self.connect("destroy", gtk.main_quit)
+	
+	
+	def _on_interval(self):
+		"""
+		Callback called at a regular interval. Update the screen.
+		"""
+		self.system.clear_cache()
+		
+		self.register_viewer.refresh()
+		for memory_viewer in self.memory_viewers:
+			memory_viewer.refresh()
+		self.control_bar.refresh()
+		
+		# Reschedule the interval
+		return True
+	
+	
+	def _on_log_update(self, widget, flag):
+		"""
+		Called when a new log item appears. If it is flagged, expand the log viewer.
+		"""
+		if flag:
+			self.log_expander.set_expanded(True)
+		
+		if self.log_expander.get_expanded():
+			# Viewer expanded
+			self.unread_log_entries = 0
+		else:
+			if flag is not None: # Flag is none on activate
+				self.unread_log_entries += 1
+		
+		
+		if self.unread_log_entries > 0:
+			self.log_expander.set_label("<b>Error Log (%d)</b>"%self.unread_log_entries)
+		else:
+			self.log_expander.set_label("Error Log")
+
+
+if __name__=="__main__":
+	from optparse import OptionParser, OptionGroup
+	parser = OptionParser()
+	
+	ser = OptionGroup(parser, "Serial Device Options",
+	                  "Options relating to the use of an serial device back-end.")
+	ser.add_option("-s", "--serial", dest = "serial_port",
+	               action="store", type="string", default = 0,
+	               help = "Use the specified serial device as the back-end")
+	ser.add_option("-b", "--baud-rate", dest = "baudrate",
+	               action="store", type="int", default = 115200,
+	               help = "Baudrate of the serial connection")
+	parser.add_option_group(ser)
+	
+	emu = OptionGroup(parser, "Emulator Options",
+	                  "Options relating to the use of an emulator back-end.")
+	emu.add_option("-e", "--emulator",
+	               dest = "emulator", action="store_true", default = False,
+	               help = "Use an emulator back-end")
+	parser.add_option_group(emu)
+	
+	# Parse arguments
+	import sys
+	options, args = parser.parse_args(sys.argv)
+	
+	if options.emulator:
+		# Emulator is passed the remaining arguments
+		back_end = EmulatorBackEnd(args[1:])
+	else:
+		back_end = SerialPortBackEnd(options.serial_port, options.baudrate)
+	
+	# Create the main window & system
+	main_window = Main(System(back_end))
+	main_window.show_all()
+	
+	# GTK Mainloop
+	glib.threads_init()
+	gtk.main()
